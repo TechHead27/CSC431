bceaa3c5d5832319258679f89c8858d639b51cf9
Cody Hunt <crhunt@calpoly.edu>
2015-05-20 10:25:33 -0700

    Added -g flag to compilation

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 TARGET_DIR=$( pwd )
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 java Compile $TARGET_DIR/$1.mini > $TARGET_DIR/$1.s
-gcc $TARGET_DIR/$1.s -o $TARGET_DIR/$1.out
+gcc -g $TARGET_DIR/$1.s -o $TARGET_DIR/$1.out
