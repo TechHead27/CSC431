565c777412f41c2af620a932b3987a9477a2f9ba
Cody Hunt <crhunt@calpoly.edu>
2015-05-19 17:16:53 -0700

    Added test script

---
+++ runTests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# First argument is directory containing benchmarks directory,
+# Second argument is compile script
+# Can also use environment variables BENCHMARK and COMPILE
+${BENCHMARK=$(readlink -f $1)}
+#if [ $? -ne 126 ]; then exit; fi
+
+shift
+${COMPILE=$(readlink -f $1)}
+#if [ $? -ne 126 ]; then exit; fi
+
+cd $BENCHMARK/benchmarks
+if [ $? -ne 0 ]; then exit; fi
+total=0
+
+for benchtest in `ls`; do
+   cd $benchtest
+   echo "Testing $benchtest..."
+   bash $COMPILE $benchtest
+   timeout --signal=KILL 5 ./$benchtest.out < input > test_out
+   diff -q output test_out
+   if [ $? -eq 0 ]; then total=$((total+1)); fi
+   cd ..
+done
+
+echo "$total tests passed."
